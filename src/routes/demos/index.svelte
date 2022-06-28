c353512abf3fb1d5c1e2a6fbba404a0e770ea09d
kamal <maroc.develop@gmail.com>
2022-06-28 01:06:13 +0100

    Add submit handler
    u can check values form on console log (line: 9), and it s better to install last version

--- src/routes/demos/index.svelte
+++ src/routes/demos/index.svelte
@@ -1,8 +1,34 @@
+<script lang="js">
+  import { valuesForm, Field } from 'svelte-formly';
+  import { get } from 'svelte/store';
+  import fieldsYou from './fieldsYou';
+  import fieldsYouDo from './fieldsYouDo';
+
+  function onSubmit() {
+    const values = get(valuesForm);
+    console.log('values', values);
+  }
+</script>
+
+<h1>Demographics</h1>
+
+<form on:submit|preventDefault={onSubmit}>
+  <h2>About You</h2>
+  <p>Information on your preferences and demographic information</p>
+  <Field fields={fieldsYou} />
+  <hr />
+  <h2>About what You Do</h2>
+  <Field fields={fieldsYouDo} />
+  <hr />
+
+  <button class="btn btn-primary" type="submit">Submit</button>
+</form>
+
 <style>
   .custom-form :global(.form-group) {
     padding: 10px;
     border: solid 1px #333;
-    margin: 5px 0; 
+    margin: 5px 0;
   }
   h2 {
     font-size: x-large;
@@ -12,26 +38,3 @@
     margin-top: 36px;
   }
 </style>
-
-<script lang="js">
-  import { valuesForm, Field } from 'svelte-formly';
-  import fieldsYou from './fieldsYou';
-  import fieldsYouDo from './fieldsYouDo';
-
-</script>
-
-<h1>Demographics</h1>
-
-
-<form>
-  <h2>About You</h2>
-  <p>Information on your preferences and demographic information</p>
-	<Field fields={fieldsYou} />
-  <hr />
-  <h2>About what You Do</h2>
-  <Field fields={fieldsYouDo} />
-  <hr />
-
-	<button class="btn btn-primary" type="submit">Submit</button>
-</form>
-
